22982db856cfe36a168074c2f675a57626636796
Aditya <Aditya@g.com>
2020-03-22 22:48:07 +0530

    update

--- js/chart.js
+++ js/chart.js
@@ -12,7 +12,14 @@ center: [78.9629, 20.5937],
 fetch("https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise")
 .then(response => response.json())
 .then(data => {
-
-const {state,active} = data.data.statewise;
-console.log(state,active)
+var myStringArray = data.data.statewise;
+var arrayLength = myStringArray.length;
+for (var i = 0; i < arrayLength; i++) {
+  //  console.log(myStringArray[i]);
+    const {state,confirmed,recovered,deaths,active} = myStringArray[i];
+    console.log(state,confirmed,recovered,deaths,active);
+    
+}
+//const {state,active} = data.data.statewise[0];
+// console.log(state,active)
 } )
