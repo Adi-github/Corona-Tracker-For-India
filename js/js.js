4c41fee1f5bd5c8094f50f10bb2d23eb082b20d1
Aditya <Aditya@g.com>
2020-03-24 00:12:30 +0530

    table addition

--- js/js.js
+++ js/js.js
@@ -14,12 +14,24 @@ fetch("https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise")
 .then(data => {
 var myStringArray = data.data.statewise;
 var arrayLength = myStringArray.length;
-for (var i = 0; i < arrayLength; i++) {
+var t = document.getElementById("table");
+/*for (var i = 0; i < arrayLength; i++) {
   //  console.log(myStringArray[i]);
     const {state,confirmed,recovered,deaths,active} = myStringArray[i];
-    console.log(state,confirmed,recovered,deaths,active);
-
+    //console.log(state,confirmed,recovered,deaths,active);
+ 
+}*/
+for (let index = 1; index < t.rows.length; index++) {
+  for (let d = 0; d < t.rows[index].cells.length; d++) {
+    t.rows[index].cells[0].innerHTML = myStringArray[index].state;
+    t.rows[index].cells[1].innerHTML = myStringArray[index].confirmed;
+    t.rows[index].cells[2].innerHTML = myStringArray[index].recovered;
+    t.rows[index].cells[3].innerHTML = myStringArray[index].deaths;
+    t.rows[index].cells[4].innerHTML = myStringArray[index].active;
+  }
+  
 }
-//const {state,active} = data.data.statewise[0];
-// console.log(state,active)
+
 } )
+
+
